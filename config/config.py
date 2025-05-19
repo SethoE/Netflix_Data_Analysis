cb6b8bd69eacbc6f2787dbdd9927265c72ea5ce9
SethoE <ehrmannsetho@gmail.com>
2025-05-19 16:09:42 +0200

    Updates Fragestellung 2

--- config/config.py
+++ config/config.py
@@ -9,6 +9,3 @@ load_dotenv(dotenv_path)
 # Zugriff auf die Tokens
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_BEARER_TOKEN = os.getenv("TMDB_BEARER_TOKEN")
-
-print(TMDB_BEARER_TOKEN)
-print(TMDB_API_KEY)
